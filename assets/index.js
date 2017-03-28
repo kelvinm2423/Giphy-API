ea080d36cc24f59973bca8d328a2ac2f8c5c7008
Kelvin Medina <kelvinm2423@gmail.com>
2017-03-27 20:26:06 -0400

    HW submission

---
+++ assets/index.js
@@ -0,0 +1,65 @@
+$("#search-button").on("click", function () {
+
+var queryURL = "http://api.giphy.com//v1/gifs/search?q=" + "" + "api_key=dc6zaTOxFJmzC";
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      //console.log(response);
+    });
+
+      var imageUrl = response.data.image_original_url;
+      var giphyImage = $("<img>");
+      giphyImage.attr("src", imageUrl);
+      giphyImage.attr("alt", "giphy image");
+      $("#images").prepend(giphyImage);
+    });
+
+// default set of buttons to populate
+var animals = ["dog", "cat", "bird"];
+
+
+$(function() {
+      arrayButtons(animals, 'newButton', '#newButton');
+
+});
+
+function arrayButtons(arrayToUse, classToAdd, areaToAddTo){
+    $(areaToAddTo).empty();
+
+    for (var i = 0; i < arrayToUse.length; i++){
+        var a = $('<button>')
+        a.addClass(classToAdd);
+        a.attr('data-type', arrayToUse[i]);
+        a.text(arrayToUse[i]);
+        $(areaToAddTo).append(a);
+    }
+
+}
+
+// controls to make it still or animate
+$(".gif").on("click", function () {
+       
+      var state = $(this).attr("data-state");
+  
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+//This is to add new buttons
+$('#addbutton').on('click', function(){
+    var newButton = $('input').eq(0).val();
+
+    if (newButton.length > 2){
+        animals.push(newButton);
+    }
+
+    arrayButtons(animals, 'newButton', '#newButton');
+
+    return false;
+});
